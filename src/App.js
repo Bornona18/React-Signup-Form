b5ec3ab81339335c3abef531f2dc1c8f300b6157
Bornona18 <61803232+Bornona18@users.noreply.github.com>
2020-08-06 17:57:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 
+import Form from './Components/Form'
 
-class Form extends Component {
-    constructor(){
-        super() ;
-            this.state = {
-                
-                username:'',
-                password:''
-            }
-        
-    }
+import './App.css';
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`submission complete for Username, Password, Occupassion, Gender, E-mail, Phone & Address`)
-    }
+function App() {
+  return (
    
-    handleChange = (event) => {
-        let {name,value} = event.target
-        this.setState({
-            [name]:value
-        })
-        console.log(this.state)
-    }
+      <div className="App">
+        
+      <Form />
+      </div>
 
     
-    render() {
-        return (
-            <div className='form'>
-            <h1>LogIn</h1>
-            <form onSubmit={this.handleSubmit}>
-                <label id = 'change'>
-                    FirstName: <input class='p' type='text' onVolumeChangeCapture={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    LastName: <input class='p'type='text' value={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    Occupassion<input class='p'type='text' value={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    Gender: <input class='p'type='text' onChange={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    {/* <input type="radio" name="Male">Male */}
-
-                    E-mail: <input class='p' type='text' value={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    Phone: <input class='p' type='text' value={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                    Address: <input class='p' type='text' value={this.handleChange.change}
-                    onChange={this.state.value} /> <br/> <br/>
-
-                </label>
-                <input id="i" type='Submit' value='submit'/>
-
-            </form>
-            </div>
-        )
-    }
-
-    
+  );
 }
 
- export default Form; 
+export default App;
